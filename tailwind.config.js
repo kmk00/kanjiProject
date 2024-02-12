2b77443a160dcf5127927b4b91d68bba3ae30fa5
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-12 16:16:40 +0100

    Extend tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#00FF75",
+        "custom-red": "#FA5F5F",
+        "custom-orange": "#FFC107",
+        "custom-bg": "#272727",
+        "custom-bg-light": "#524D4D",
+        "custom-white": "#F0E7E7",
+      },
+    },
   },
   plugins: [],
 };
