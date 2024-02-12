6c947201d55e3a5dca0ba40fe0ec344cc0f5fa57
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-12 16:43:11 +0100

    Create Header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
-<template>
+<script setup>
+import {  RouterLink,RouterView } from 'vue-router';
 
+</script>
+
+<template>
+  <div>
+      <nav class="mx-auto w-3/4 max-w-[1024px] flex justify-center gap-10 xl:gap-60 p-4 border-custom-orange border-b-2">
+        <RouterLink to="/">QUIZ</RouterLink>
+        <RouterLink to="/wordslist">WORDS LIST</RouterLink>
+      </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
+import QuizViewVue from "@/views/QuizView.vue";
+import WordsListViewVue from "@/views/WordsListView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "quiz",
+      component: QuizViewVue,
+    },
+    {
+      path: "/wordslist",
+      name: "wordslist",
+      component: WordsListViewVue,
+    },
+  ],
 });
 
 export default router;
