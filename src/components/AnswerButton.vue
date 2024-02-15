b855c049e18bff5a25fc5f4ee92a0cc09346c98f
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-15 17:11:21 +0100

    Add english mode

--- src/components/AnswerButton.vue
+++ src/components/AnswerButton.vue
@@ -3,33 +3,32 @@ import { ref } from 'vue';
 const { correctAnswerIndex, questionIndex,  selectFrom, selectTo, quiz,keyid  } = defineProps(["correctAnswerIndex", "questionIndex", "selectFrom", "selectTo", "quiz","keyid"])
 
 const isCorrect = ref(null);
-
+const isAnswered = ref(false)
 
 const selectAnswer = (e) => {
+    isAnswered.value = true
     if (e.currentTarget.id == correctAnswerIndex) isCorrect.value = true 
-    console.log("\ncorrect answer index",correctAnswerIndex)
-    console.log("button index", e.currentTarget.id)
-    console.log("isCorrect",isCorrect.value)
-    console.log("correct answer index", questionIndex)
-    console.log("key",keyid)
-
+    console.log(isAnswered.value, isCorrect.value)
 }
 
 </script>
 
 <template>
     <div>
-        keyid{{ keyid }}<br>
-        correct answer index {{ correctAnswerIndex }}<br>
-        question index {{ questionIndex }}
         <button :id="questionIndex" 
-        :style="{backgroundColor: isCorrect ? 'green' : 'transparent'}" 
+        :style="isAnswered && {backgroundColor: isCorrect ? '#0dd8549e' : '#923838d6'}" 
         @click="selectAnswer" v-if="selectTo === 'english'"  
         class="min-h-20 w-full p-2 hover:bg-custom-orange/10 transition-all border rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer ">
             <p v-for="meaningIndex in quiz.questionsArray[questionIndex].meanings.length ">{{ quiz.questionsArray[questionIndex].meanings[meaningIndex-1]}}</p>
         </button>
+        <button :id="questionIndex"
+            :style="isAnswered && { backgroundColor: isCorrect ? '#2dc8639e' : '#923838d6' }" 
+            @click="selectAnswer" v-if="selectTo === 'kanji'"  
+            class="min-h-20 w-full disa p-2 hover:bg-custom-orange/20 transition-all border rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer ">
+                <p class="md:text-5xl">{{ quiz.questionsArray[questionIndex].kanji }}</p>
+            </button>
         <button :id="questionIndex" 
-        :style="{backgroundColor: isCorrect ? 'green' : 'transparent'}" 
+        :style="isAnswered && { backgroundColor: isCorrect ? 'green' : '#923838d6' }" 
         @click="selectAnswer" v-if="selectTo === 'hiragana'"  
         class="min-h-20 w-full p-2 hover:bg-custom-orange/10 transition-all border rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer ">
         <div class="grid grid-cols-2">
