213980d1958a050f66d70c7c5606997a42f93965
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-16 12:51:21 +0100

    Update QuestionPanel.vue

--- src/components/QuestionPanel.vue
+++ src/components/QuestionPanel.vue
@@ -2,31 +2,39 @@
     import FinalScore from './FinalScore.vue';
     const { isGame, quiz, options } = defineProps(["isGame", "quiz", "options"])
 
+
+
 </script>
 
 
 
 <template>
-    <div class="mt-6 p-4 min-h-40 select-none md:min-h-80 max-w-[600px] flex justify-center items-center border rounded-md bg-custom-bg border-blue-gray-200">
+    <div
+        class="mt-6 p-4 min-h-40 select-none md:min-h-80 max-w-[600px] flex justify-center items-center border rounded-md bg-custom-bg border-blue-gray-200">
         <p v-if="!isGame" class="text-xl animate-fade-in md:text-4xl">Please select the options</p>
-        <p v-else-if="isGame && options.selectFrom === 'kanji' && quiz.currentQuestionIndex < quiz.questionsArray.length" class="text-3xl animate-fade-in md:text-8xl">{{ quiz.questionsArray[quiz.currentQuestionIndex].kanji }}</p>
-        <div v-else-if="isGame && options.selectFrom === 'hiragana' && quiz.currentQuestionIndex < quiz.questionsArray.length" >
+        <p v-else-if="isGame && options.selectFrom === 'kanji' && quiz.currentQuestionIndex < quiz.questionsArray.length"
+            class="text-3xl animate-fade-in md:text-8xl">{{ quiz.questionsArray[quiz.currentQuestionIndex].kanji }}</p>
+        <div
+            v-else-if="isGame && options.selectFrom === 'hiragana' && quiz.currentQuestionIndex < quiz.questionsArray.length">
             <div class="grid animate-fade-in gap-10 md:text-2xl grid-cols-2">
                 <div class="flex flex-col">
                     <p class="border-b border-custom-bg-light/85">on</p>
-                    <p v-for="meaningIndex in quiz.questionsArray[quiz.currentQuestionIndex].readings_on.length ">{{ quiz.questionsArray[quiz.currentQuestionIndex].readings_on[meaningIndex - 1] }}</p>
+                    <p v-for="meaningIndex in quiz.questionsArray[quiz.currentQuestionIndex].readings_on.length ">{{
+                        quiz.questionsArray[quiz.currentQuestionIndex].readings_on[meaningIndex - 1] }}</p>
                 </div>
                 <div class="flex flex-col">
                     <p class="border-b border-custom-bg-light/85">kun</p>
-                    <p v-for="meaningIndex in quiz.questionsArray[quiz.currentQuestionIndex].readings_kun.length ">{{ quiz.questionsArray[quiz.currentQuestionIndex].readings_kun[meaningIndex - 1] }}</p>
+                    <p v-for="meaningIndex in quiz.questionsArray[quiz.currentQuestionIndex].readings_kun.length ">{{
+                        quiz.questionsArray[quiz.currentQuestionIndex].readings_kun[meaningIndex - 1] }}</p>
                 </div>
             </div>
         </div>
-        <div v-else-if="isGame && options.selectFrom === 'english' && quiz.currentQuestionIndex < quiz.questionsArray.length" >
+        <div
+            v-else-if="isGame && options.selectFrom === 'english' && quiz.currentQuestionIndex < quiz.questionsArray.length">
             <div class="flex animate-fade-in flex-col md:text-6xl">
-                <p v-for="meaningIndex in quiz.questionsArray[quiz.currentQuestionIndex].readings_on.length ">{{ quiz.questionsArray[quiz.currentQuestionIndex].meanings[meaningIndex - 1] }}</p>
-            </div>
+                <p v-for="meaningIndex in quiz.questionsArray[quiz.currentQuestionIndex].readings_on.length ">{{
+                quiz.questionsArray[quiz.currentQuestionIndex].meanings[meaningIndex - 1] }}</p>
         </div>
-        <FinalScore v-else :total-questions="quiz.questionsArray.length"/>
     </div>
-</template>
+    <FinalScore v-else :total-questions="quiz.questionsArray.length" />
+</div></template>
