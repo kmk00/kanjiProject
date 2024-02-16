c024a3c99f3d1b58caa685167528ff41bf01e8b2
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-16 13:40:10 +0100

    Add quiz reset functionality
    With same and different questions

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,175 +1,179 @@
 <script setup>
-import GradeSelector from '@/components/GradeSelector.vue';
-import { reactive, ref, watch } from "vue";
-import words  from '@/assets/kanji.json';
-import AnswerButton from '@/components/AnswerButton.vue';
-import {uuid} from 'vue-uuid';
-import { useScoreStore } from '@/stores/score';
-import QuestionPanel from '@/components/QuestionPanel.vue';
-import ProgressBar from '@/components/ProgressBar.vue';
-import {shuffleArray} from '@/utils/useful'
+    import { reactive, ref, watch } from "vue";
+    import {uuid} from 'vue-uuid';
+    import words  from '@/assets/kanji.json';
+    import { useScoreStore } from '@/stores/score';
+    import {shuffleArray} from '@/utils/useful'
 
-// Global score register right and wrong answers
-const scoreStore = useScoreStore()
-// Check if game is in progress
-const isGame = ref(false)
-// Stores error message
-const errorMessage = ref('')
+    import GradeSelector from '@/components/GradeSelector.vue';
+    import AnswerButton from '@/components/AnswerButton.vue';
+    import QuestionPanel from '@/components/QuestionPanel.vue';
+    import ProgressBar from '@/components/ProgressBar.vue';
 
-// Keep track of selected grades in form options 
-let gradesSelected = reactive([{ 1: false }, { 2: false }, { 3: false }, { 4: false }, { 5: false }, { 6: false }, { 7: false }, { 8: false }])
+    // Global score register right and wrong answers
+    const scoreStore = useScoreStore()
+    // Check if game is in progress
+    const isGame = ref(false)
+    // Stores error message
+    const errorMessage = ref('')
 
-// Table of words filtered by grade
-let filteredWordsByGrade = ref([])
+    // Keep track of selected grades in form options 
+    let gradesSelected = reactive([{ 1: false }, { 2: false }, { 3: false }, { 4: false }, { 5: false }, { 6: false }, { 7: false }, { 8: false }])
 
-// Quiz options
-let options = reactive({
-    grades: gradesSelected,
-    selectFrom: '',
-    selectTo: '',  
-})
+    // Table of words filtered by grade
+    let filteredWordsByGrade = ref([])
 
-// Quiz variables
-let quiz = ref({
-    numberOfQuestions: null,
-    currentQuestionIndex: null,
-    questionsArray: [],
-    answersIndexes: [],
-    uniqueIndexes: []
-})
+    // Quiz options
+    let options = reactive({
+        grades: gradesSelected,
+        selectFrom: '',
+        selectTo: '',  
+    })
 
-
-const selectGrade = (grade) => {
-    //Grade selection in form - GradeSelector component 
-    gradesSelected[grade - 1][grade] = !gradesSelected[grade - 1][grade]
-}
-
-watch(() => quiz.value.currentQuestionIndex, () => {
-    // Keeps track of current question index and 
-    if (quiz.value.currentQuestionIndex >= quiz.value.questionsArray.length) {
-        return
-    }    
-
-    // Prepares answers indexes
-    prepareAnswersIndexes()
-    
-    // Prepares unique indexes for answer buttons when the question changes 
-    // Essential when the same answer which was correct is shown again in the next question as wrong 
-    for (let i = 0; i < quiz.value.answersIndexes.length; i++) {
-        quiz.value.uniqueIndexes.push(uuid.v4());
-    }
-
-})
-
-
-const selectOptions = () => {
-    //Validation of selected options
-    if (checkIfGradesInOptionSelected() == false || options.selectFrom == '' || options.selectTo == '') {
-        errorMessage.value = 'Please provide all options.'
-        return
-    }
-
-    if(options.selectFrom === options.selectTo){
-        errorMessage.value = 'Please provide different options.'
-        return
-    }
-    errorMessage.value = ''
-
-    //Create array of selected grades
-    let gradesToFilter = []
-    for (let i = 0; i < options.grades.length; i++) {
-        if (Object.values(options.grades[i])[0]) {
-            gradesToFilter.push(i + 1);
-        }
-    }
-
-    // Filter words by selected grades
-    filteredWordsByGrade.value = Object.values(words).filter(word => gradesToFilter.includes(word.grade))
-        .map(word => ({ ...word, kanji: Object.keys(words).find(key => words[key] === word) }));   
-}
-
-const checkIfGradesInOptionSelected = () => {
-    // Check if any grade is selected
-    const anySelected = Object.values(options.grades).some((grade) => Object.values(grade).some((status) => status === true))
-    return anySelected
-}
-
-const clearOptions = () => {
-    // Clear all options
-    isGame.value = false
-    options.selectFrom = ''
-    options.selectTo = ''
-    gradesSelected = [{ 1: false }, { 2: false }, { 3: false }, { 4: false }, { 5: false }, { 6: false }, { 7: false }, { 8: false }]
-    options.grades = gradesSelected
-    filteredWordsByGrade.value = []
-    errorMessage.value = ''
-    quiz.value = {
+    // Quiz variables
+    let quiz = ref({
         numberOfQuestions: null,
-        questionsArray: [],
         currentQuestionIndex: null,
+        questionsArray: [],
         answersIndexes: [],
         uniqueIndexes: []
+    })
+
+    watch(() => quiz.value.currentQuestionIndex, () => {
+        // Keeps track of current question index and 
+        if (quiz.value.currentQuestionIndex >= quiz.value.questionsArray.length) {
+            return
+        }    
+
+        // Prepares answers indexes
+        prepareAnswersIndexes()
+        
+        // Prepares unique indexes for answer buttons when the question changes 
+        // Essential when the same answer which was correct is shown again in the next question as wrong 
+        for (let i = 0; i < quiz.value.answersIndexes.length; i++) {
+            quiz.value.uniqueIndexes.push(uuid.v4());
+        }
+
+    })
+
+    const selectGrade = (grade) => {
+        //Grade selection in form - GradeSelector component 
+        gradesSelected[grade - 1][grade] = !gradesSelected[grade - 1][grade]
     }
-    scoreStore.resetScore()
+
+    const selectOptions = () => {
+        //Validation of selected options
+        if (checkIfGradesInOptionSelected() == false || options.selectFrom == '' || options.selectTo == '') {
+            errorMessage.value = 'Please provide all options.'
+            return
+        }
+
+        if(options.selectFrom === options.selectTo){
+            errorMessage.value = 'Please provide different options.'
+            return
+        }
+        errorMessage.value = ''
+
+        //Create array of selected grades
+        let gradesToFilter = []
+        for (let i = 0; i < options.grades.length; i++) {
+            if (Object.values(options.grades[i])[0]) {
+                gradesToFilter.push(i + 1);
+            }
+        }
+
+        // Filter words by selected grades
+        filteredWordsByGrade.value = Object.values(words).filter(word => gradesToFilter.includes(word.grade))
+            .map(word => ({ ...word, kanji: Object.keys(words).find(key => words[key] === word) }));   
+    }
+
+    const checkIfGradesInOptionSelected = () => {
+        // Check if any grade is selected
+        const anySelected = Object.values(options.grades).some((grade) => Object.values(grade).some((status) => status === true))
+        return anySelected
+    }
+
+    const clearOptions = () => {
+        // Clear all options
+        isGame.value = false
+        options.selectFrom = ''
+        options.selectTo = ''
+        gradesSelected = [{ 1: false }, { 2: false }, { 3: false }, { 4: false }, { 5: false }, { 6: false }, { 7: false }, { 8: false }]
+        options.grades = gradesSelected
+        filteredWordsByGrade.value = []
+        errorMessage.value = ''
+        quiz.value = {
+            numberOfQuestions: null,
+            questionsArray: [],
+            currentQuestionIndex: null,
+            answersIndexes: [],
+            uniqueIndexes: []
+        }
+        scoreStore.resetScore()
+    }
+
+    const initialiseQuiz = () => {
+        // Validation of number of questions
+        if (quiz.value.numberOfQuestions < 4 || quiz.value.numberOfQuestions > filteredWordsByGrade.value.length) {
+            errorMessage.value = 'Please provide valid number of questions.'
+            return
+        }
+
+        errorMessage.value = ''
+
+        isGame.value = true
+
+        quizToBeginning()
+        quiz.value.questionsArray = pickQuestionsToQuiz(filteredWordsByGrade.value, quiz.value.numberOfQuestions)
 }
 
-const initialiseQuiz = () => {
-    // Validation of number of questions
-    if (quiz.value.numberOfQuestions < 4 || quiz.value.numberOfQuestions > filteredWordsByGrade.value.length) {
-        errorMessage.value = 'Please provide valid number of questions.'
-        return
+    const quizToBeginning = () => {
+        scoreStore.resetScore()
+        quiz.value.questionsArray = shuffleArray(quiz.value.questionsArray)
+        quiz.value.currentQuestionIndex = 0
     }
 
-    errorMessage.value = ''
+    const pickQuestionsToQuiz = (questionsArray,number) => {
+        // Pick questions to quiz based on number of questions and save it in an array
 
-    isGame.value = true
-    quiz.value.currentQuestionIndex = 0
-    quiz.value.questionsArray = pickQuestionsToQuiz(filteredWordsByGrade.value, quiz.value.numberOfQuestions)
+        const arrayCopy = questionsArray.slice();
+
+        const pickedQuestions = [];
+        for (let i = 0; i < number; i++) {
+            const randomIndex = Math.floor(Math.random() * arrayCopy.length);
+            pickedQuestions.push(arrayCopy[randomIndex]);
+            arrayCopy.splice(randomIndex, 1);
+        }
+        return pickedQuestions
+    }
+
+    const prepareAnswersIndexes = () => {
+        // Prepares answers indexes for current question
+        quiz.value.answersIndexes = [];
+        quiz.value.answersIndexes.push(quiz.value.currentQuestionIndex);
+        for (let i = 0; i < 3; i++) {
+            let index;
+            do {
+                index = Math.floor(Math.random() * quiz.value.questionsArray.length);
+            } while (quiz.value.answersIndexes.includes(index)); 
+            quiz.value.answersIndexes.push(index);
+        }
+        quiz.value.answersIndexes = shuffleArray(quiz.value.answersIndexes);
+
+    }
+
+    const nextQuestion = () => {
+        // Prepares next question
+        if (quiz.value.currentQuestionIndex >= quiz.value.questionsArray.length) {
+            return
+        }
+
+        quiz.value.uniqueIndexes = [];
+        quiz.value.currentQuestionIndex++
+}
+
+
     
-}
-
-const pickQuestionsToQuiz = (questionsArray,number) => {
-    // Pick questions to quiz based on number of questions and save it in an array
-
-    const arrayCopy = questionsArray.slice();
-
-    const pickedQuestions = [];
-    for (let i = 0; i < number; i++) {
-        const randomIndex = Math.floor(Math.random() * arrayCopy.length);
-        pickedQuestions.push(arrayCopy[randomIndex]);
-        arrayCopy.splice(randomIndex, 1);
-    }
-    return pickedQuestions
-}
-
-const prepareAnswersIndexes = () => {
-    // Prepares answers indexes for current question
-    quiz.value.answersIndexes = [];
-    quiz.value.answersIndexes.push(quiz.value.currentQuestionIndex);
-    for (let i = 0; i < 3; i++) {
-        let index;
-        do {
-            index = Math.floor(Math.random() * quiz.value.questionsArray.length);
-        } while (quiz.value.answersIndexes.includes(index)); 
-        quiz.value.answersIndexes.push(index);
-    }
-    quiz.value.answersIndexes = shuffleArray(quiz.value.answersIndexes);
-
-}
-
-const nextQuestion = () => {
-    // Prepares next question
-    if (quiz.value.currentQuestionIndex >= quiz.value.questionsArray.length) {
-        return
-    }
-
-    quiz.value.uniqueIndexes = [];
-    quiz.value.currentQuestionIndex++
-}
-
-
-
-
 </script>
 
 <template>
@@ -198,21 +202,58 @@ const nextQuestion = () => {
         </form>
         <div v-if="filteredWordsByGrade.length > 0" class="animate-fade-in mt-4">
             <p v-if="!isGame" class="text-xl p-2">How many words do you want?</p>
-            <p v-else>{{ quiz.currentQuestionIndex }} / {{ quiz.questionsArray.length}}</p>
+            <p v-else-if="isGame && quiz.currentQuestionIndex < quiz.questionsArray.length">{{ quiz.currentQuestionIndex }} / {{ quiz.questionsArray.length}}</p>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
-                <input v-if="!isGame" type="number" v-model="quiz.numberOfQuestions" :disabled="isGame" class="h-full p-2 border rounded-md disabled:text-gray-500 bg-custom-bg border-blue-gray-200 cursor-pointer " :placeholder="'From 4 to ' + filteredWordsByGrade.length">
-                <ProgressBar v-else :quiz="quiz"/>
+                <input 
+                    v-if="!isGame" 
+                    type="number" 
+                    v-model="quiz.numberOfQuestions" 
+                    :disabled="isGame" 
+                    class="h-full p-2 border rounded-md disabled:text-gray-500 bg-custom-bg border-blue-gray-200 cursor-pointer " 
+                    :placeholder="'From 4 to ' + filteredWordsByGrade.length">
+                <ProgressBar 
+                    v-if="isGame && quiz.currentQuestionIndex < quiz.questionsArray.length" 
+                    :quiz="quiz"/>
+                <button v-if="isGame && quiz.currentQuestionIndex >= quiz.questionsArray.length" 
+                    @click="initialiseQuiz" 
+                    class="p-2 border hover:bg-custom-orange/10 transition-all rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer"
+                    >Try again with different set</button>
                 <div class="grid grid-cols-2 gap-2">
-                    <button v-if="!isGame" @click="initialiseQuiz" class="p-2 border hover:bg-custom-orange/10 transition-all rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer">Start</button>
-                    <button v-else @click="nextQuestion" class="p-2 border hover:bg-custom-orange/10 transition-all rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer">Next Question</button>
-                    <button @click="clearOptions" class="p-2 border rounded-md hover:bg-custom-orange/10 transition-all bg-custom-bg border-blue-gray-200 cursor-pointer">Reset</button>
+                    <button 
+                        v-if="!isGame" 
+                        @click="initialiseQuiz" 
+                        class="p-2 border hover:bg-custom-orange/10 transition-all rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer"
+                        >Start</button>
+                    <button 
+                        v-else-if="isGame && quiz.currentQuestionIndex < quiz.questionsArray.length" 
+                        @click="nextQuestion" 
+                        class="p-2 border hover:bg-custom-orange/10 transition-all rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer"
+                        >Next Question</button>
+                    <button 
+                        v-else-if="isGame && quiz.currentQuestionIndex >= quiz.questionsArray.length" 
+                        @click="quizToBeginning" 
+                        class="p-2 border hover:bg-custom-orange/10 transition-all rounded-md bg-custom-bg border-blue-gray-200 cursor-pointer"
+                        >Restart</button>
+                    <button 
+                        @click="clearOptions" 
+                        class="p-2 border rounded-md hover:bg-custom-orange/10 transition-all bg-custom-bg border-blue-gray-200 cursor-pointer"
+                        >Options</button>
                 </div>
             </div>
         </div>
         <p v-if="errorMessage" class="mt-2 text-red-500">{{ errorMessage }}</p>
         <QuestionPanel :is-game="isGame" :quiz="quiz" :options="options" />
         <div class="grid md:mt-8 mt-4 grid-cols-2 gap-2">
-            <AnswerButton v-if="isGame && quiz.currentQuestionIndex < quiz.questionsArray.length" v-for="(questionIndex, id) in quiz.answersIndexes" :key="quiz.uniqueIndexes[id]" :keyid="quiz.uniqueIndexes[id]" :correctAnswerIndex="quiz.currentQuestionIndex" :questionIndex="questionIndex" :selectFrom="options.selectFrom" :selectTo="options.selectTo" :quiz="quiz"/>
+            <AnswerButton 
+                v-if="isGame && quiz.currentQuestionIndex < quiz.questionsArray.length" 
+                v-for="(questionIndex, id) in quiz.answersIndexes" 
+                :key="quiz.uniqueIndexes[id]" 
+                :keyid="quiz.uniqueIndexes[id]" 
+                :correctAnswerIndex="quiz.currentQuestionIndex" 
+                :questionIndex="questionIndex" 
+                :selectFrom="options.selectFrom" 
+                :selectTo="options.selectTo" 
+                :quiz="quiz"/>
         </div>
     </div>
 </template>
